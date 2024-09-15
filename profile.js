f7ab95a88c7e9f660f1f0766b7035f846b7b7ebc
ni3rav <mahetan35@gmail.com>
2024-09-16 00:30:06 +0530

    cleaning files

--- profile.js
+++ profile.js
@@ -1,15 +1,15 @@
 // profile.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    const searchToggle = document.getElementById('search-toggle');
-    const popupMenu = document.getElementById('popup-menu');
-    const closePopup = document.getElementById('close-popup');
+document.addEventListener("DOMContentLoaded", function () {
+  const searchToggle = document.getElementById("search-toggle");
+  const popupMenu = document.getElementById("popup-menu");
+  const closePopup = document.getElementById("close-popup");
 
-    searchToggle.addEventListener('click', function() {
-        popupMenu.style.display = 'block';
-    });
+  searchToggle.addEventListener("click", function () {
+    popupMenu.style.display = "block";
+  });
 
-    closePopup.addEventListener('click', function() {
-        popupMenu.style.display = 'none';
-    });
+  closePopup.addEventListener("click", function () {
+    popupMenu.style.display = "none";
+  });
 });
